6370a2ac43e0abe8232b1ba13dc4b414faff7026
Sagar <sagar@debian>
2023-05-04 23:15:18 +0545

    merged and conflict resolved

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 function App(){
   return (
     <>
-<<<<<<< HEAD
+
     <p>THsi is test for git branchin gand merging</p>
-=======
-      <p>This is  test branch to test mering and branching.</p>
->>>>>>> testbranch
+
     </>
   );
 }
