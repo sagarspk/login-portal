dc42d934b148b9a32980bf74553a4c36c3c9f94a
Sagar <sagar@debian>
2023-09-16 18:43:28 +0545

    frontend updated

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Home from "./components/Home"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <Home />
+    <Footer />
+    </>
+  )
+}
+
+export default App
